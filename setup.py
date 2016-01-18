0fd40bd7ca547b364cd9b4a19a157c47d3dea8d5
Jeff Ploughman <j.j.ploughman@gmail.com>
2016-01-18 12:34:20 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='govern8rLib',
-        version='0.0.10',
+        version='0.0.11',
         packages=[''],
         url='https://github.com/cypherhat/govern8rLib',
         license='',
