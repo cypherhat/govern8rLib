33a00b7c828e74cfef173c7452e5d8577fb5f651
raju <rajumail@gmail.com>
2016-01-25 19:52:29 -0500

    First Cut Notary Client Class. Main API for UI.

--- wallet.py
+++ wallet.py
@@ -12,3 +12,12 @@ def create_wallet(wallet_type, key, logger):
         wallet = PlainWallet()
     wallet.instance()
     return wallet
+
+def load_wallet(wallet_type, key, logger):
+    if wallet_type == 'ServerWallet':
+        wallet = ServerWallet(key, logger)
+    elif wallet_type == 'ClientWallet':
+        wallet = ClientWallet(key)
+    else:
+        wallet = PlainWallet()
+    return wallet
