883ab780b77419ab73d9a104640a7de65bf00f32
raju <rajumail@gmail.com>
2016-01-29 13:16:38 -0500

    Fixed logger into self.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='govern8rLib',
-        version='0.0.29',
+        version='0.0.32',
         packages=[''],
         url='https://github.com/cypherhat/govern8rLib',
         license='',
