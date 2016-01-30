867d13e42b1214837f030b27cbd3ccabc1d51acf
Jeff Ploughman <j.j.ploughman@gmail.com>
2016-01-30 08:23:35 -0500

    Removed load wallet

--- wallet.py
+++ wallet.py
@@ -3,18 +3,9 @@ from client_wallet import ClientWallet
 from plain_wallet import PlainWallet
 
 
+# This is a factory method to instantiate a wallet. The wallet constructor handles the logic
+# of determining whether a wallet needs to be created from scratch.
 def create_wallet(wallet_type, key, logger):
-    if wallet_type == 'ServerWallet':
-        wallet = ServerWallet(key, logger)
-    elif wallet_type == 'ClientWallet':
-        wallet = ClientWallet(key)
-    else:
-        wallet = PlainWallet()
-    wallet.instance()
-    return wallet
-
-
-def load_wallet(wallet_type, key, logger):
     if wallet_type == 'ServerWallet':
         wallet = ServerWallet(key, logger)
     elif wallet_type == 'ClientWallet':
